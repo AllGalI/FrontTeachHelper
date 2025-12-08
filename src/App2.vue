5eccdc15d65b4fa03ad5a1f8e61575306ec4c06b
AlixanGalachiev <all.gall1@mail.ru>
2025-12-08 23:35:28 +0300

    move navBar to footer
    appended toggle Menu

--- src/App2.vue
+++ src/App2.vue
@@ -1,107 +1,90 @@
 <script setup>
+  import { onUnmounted, ref, useTemplateRef } from 'vue';
+
+  const isOpen = ref(false);
+
+  const menu = useTemplateRef('menuRef');
+  const menuItems = ref([
+    {
+      'text': "About",
+      "route": "/about"
+    },
+    {
+      'text': "Billings",
+      "route": "/billings"
+    },
+    {
+      'text': "Login",
+      "route": "/login"
+    }
+  ])
+
+  function toggleMenu() {
+    isOpen.value = !isOpen.value;
+  }
+
+  // function 
+
+  function closeOnOutsideClick(event) {
+    if (
+      isOpen.value
+      && !menu.value.contains(event.target)
+    ) {
+      isOpen.value = false;
+    }
+  }
+
+  document.addEventListener('click', closeOnOutsideClick);
+
+  onUnmounted(() => {
+    document.removeEventListener('click', closeOnOutsideClick);
+  })
+
 </script>
 
 <template>
   <div class="app" id="app">
-    <header class="nav">
+    <nav
+      class="menu"
+      :class="{'menu--open': isOpen}"
+    >
+      <ul
+        class="menu__list"
+        ref="menuRef"
+      >
+        <li
+          class="menu__item"
+          v-for="item in menuItems"> {{ item.text }}
+        </li>
+      </ul>
+    </nav>
+    <footer class="nav nav__bottom">
       <div class="nav__bar" >
         <div class="nav__logo">
           <img src="@/assets/logo.svg" alt="logo" class="logo">
           <div class="logo__text">Teach Helper</div>
         </div>
-        <button class="hamburger__button">
-          <div class="hamburger__container">
+        <button 
+          @click.stop="toggleMenu"
+          class="hamburger__button"
+          ref="menuButton"
+        >
+          <div 
+            class="hamburger__container"
+            :class="{'hamburger__container--open': isOpen}"
+          >
             <div class="hamburger__item-top"></div>
             <div class="hamburger__item-middle"></div>
             <div class="hamburger__item-bottom"></div>
           </div>
-        </button>
-        <!-- <nav class="nav__list">
-          <li></li>
-        </nav> -->
-      </div>
-    </header>
-
-    <div class="Localnav row">
-      <span class="ss">alks;djf;askljdf</span>
-      <span class="ss">alks;djf;askljdf</span>
-    </div>
-    <div class="page column">
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-      <p>asdf</p>
-      <p>asdf</p>
-      <p>asdfvv</p>
-    </div>
-    <footer class="nav nav__bottom">
-      <div class="nav__bar">
-        <div class="nav__logo">
-          <img src="@/assets/logo.svg" alt="logo" class="logo">
-          <div class="logo__text">Teach Helper</div>
-        </div>
-        <!-- <div class="row">
-          <p>About</p>
-          <p>Smth</p>
-          <p>Smth2</p>
-        </div> -->
+        </button>        
       </div>
     </footer>
   </div>
 </template>
 
 <style>
-.ss {
-  line-height: 47px;
-}
+
+
+
 </style>
